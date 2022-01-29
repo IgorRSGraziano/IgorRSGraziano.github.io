8a3f747de613e9a5b82f903a7571b413e4eb2515
IgorRSGraziano <igorrsgraziano@gmail.com>
2022-01-29 18:35:14 -0300

    fix: pseudo-element disappearing in mobile

--- modules/mainContainerHighlight.js
+++ modules/mainContainerHighlight.js
@@ -1,12 +1,14 @@
 export default function mainContainerHighlight() {
     addEventListener('mousemove', element => {
-        document.querySelector('[data-main-container]')
-        const mouseHoverElement = element.target
-        if (mouseHoverElement.closest('[data-main-container]') !== null && !document.querySelector('[data-container-highlighter="true"]')) {
-            document.querySelectorAll('[data-main-container]').forEach(el => {
-                el == mouseHoverElement ? null : el.dataset.mainContainer = 'false'
-            })
-            mouseHoverElement.closest('[data-main-container]').dataset.mainContainer = 'true';
+        if (innerWidth > 900) {
+            document.querySelector('[data-main-container]')
+            const mouseHoverElement = element.target
+            if (mouseHoverElement.closest('[data-main-container]') !== null && !document.querySelector('[data-container-highlighter="true"]')) {
+                document.querySelectorAll('[data-main-container]').forEach(el => {
+                    el == mouseHoverElement ? null : el.dataset.mainContainer = 'false'
+                })
+                mouseHoverElement.closest('[data-main-container]').dataset.mainContainer = 'true';
+            }
         }
     })
 }
